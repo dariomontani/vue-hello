03fb8e9a833e80003048756b3663212a27430e65
dariomontani <dariomontani@gmail.com>
2021-12-15 15:01:42 +0100

    add message h1

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@
 // Inseriamo un input per visualizzare in pagina un testo inserito da un utente.
 // Inseriamo una immagine e accanto un numero che identifica il numero di prodotti desiderati, i dati devono venire da Vue.
 // Poi aggiungiamo un pulsante per aumentare questo numero.
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Vue Hello!',
+        image: 'https://picsum.photos/200/300'
+    }
+})
